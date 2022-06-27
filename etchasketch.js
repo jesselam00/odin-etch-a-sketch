a0c254b130eeb7bcf991608b7b010cfd010de9d8
jesselam00 <yahuy3177@gmail.com>
2022-06-27 14:38:05 -0500

    Add hover effect

--- etchasketch.js
+++ etchasketch.js
@@ -1,22 +1,20 @@
 const containerDiv = document.querySelector(".container")
 const numberOfSquaresX = 16
 const numberOfSquaresY = 16
-const squareDivHeight = "20px"
+const squareDivHeight = 40
 
 const numberOfSquares = numberOfSquaresX * numberOfSquaresY
-const containerDivHeight = `${numberOfSquaresX * 20}px`
-const containerDivWidth = `${numberOfSquaresY * 20}px`
-console.log(containerDivHeight)
+const containerDivHeight = `${numberOfSquaresX * squareDivHeight}px`
+const containerDivWidth = `${numberOfSquaresY * squareDivHeight}px`
 containerDiv.style.height = containerDivHeight
 containerDiv.style.width = containerDivWidth
 
 for (let i = 0; i < numberOfSquares; i++) {
     const squareDiv = document.createElement("div")
-    squareDiv.style.backgroundColor = "white"
-    squareDiv.style.width = squareDivHeight
-    squareDiv.style.height = squareDivHeight
-    squareDiv.style.borderStyle = "solid"
-    squareDiv.style.borderWidth = "1px"
+    squareDiv.classList.add("squareDiv")
+    squareDiv.style.width = `${squareDivHeight}px`
+    squareDiv.style.height = `${squareDivHeight}px`
+    squareDiv.style.borderWidth = "0.1px"
     containerDiv.appendChild(squareDiv)
 }
 
